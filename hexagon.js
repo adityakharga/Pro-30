ee52aca7b5848c6b5fd96ce6ac5e4b5847e299c9
adityakharga <72646575+adityakharga@users.noreply.github.com>
2021-01-22 20:58:12 +0530

    Update hexagon.js

--- hexagon.js
+++ hexagon.js
@@ -1,27 +1,39 @@
 class Hexagon{
     constructor(x,y,width,height){
-      var options={
-        isStatic : false,
-        restitution : 0.8,
-        friction : 0.3,
-        density : 0.8
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options)
-      this.width = width
-      this.height = height
+    var options={
+      'isStatic' : false,
+      'restitution' : 0.08,
+      'friction' : 0.3
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options)
+    this.width = width
+    this.height = height
 
-      this.image = loadImage("polygon.png")
-  
-      World.add(world,this.body)
+    this.Visiblity = 255
+
+    World.add(world,this.body)
+  }
     }
     display(){
-      var pos = this.body.position
-      var angle = this.body.angle
-      push();
-      translate(pos.x,pos.y)
-      rotate(angle)
-      imageMode(CENTER)
-      image(this.image,0,0,40,40)
-      pop();
+    if(this.body.speed < 3){
+     
+    var pos = this.body.position
+    var angle = this.body.angle
+    push();
+    translate(pos.x,pos.y)
+    rotate(angle)
+    rectMode(CENTER)
+    stroke("black")
+    fill("white")
+    rect(0,0,this.width,this.height)
+    pop();
     }
+    else{
+    World.remove(world,this.body)
+    push();
+    this.Visiblity = this.Visiblity-5
+    tint(255,this.Visiblity)
+    pop();
+    }
+  }
   }
